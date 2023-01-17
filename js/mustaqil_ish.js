46f9398387b5ad6db95eede53341984a2d30c6ba
SabriddinJalolov <sabriddinjalolov1@gmail.com>
2023-01-18 00:02:33 +0500

    masala

---
+++ js/mustaqil_ish.js
@@ -0,0 +1,10 @@
+let numberOne = 70 ,numberTwo = 60;
+if(numberOne>numberTwo){
+    console.log("KAttasi:",numberOne);
+}else if(numberOne<numberTwo){
+    console.log("Kattasi:",numberTwo);
+}else{
+    console.log("Ikkala sson ham teng");
+}
+
+// sonni katta yoki katta yoki kichikligini topib beradi

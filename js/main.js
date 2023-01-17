e17f469fe94a976d509afd2958dc6e4e781129a3
SabriddinJalolov <sabriddinjalolov1@gmail.com>
2023-01-17 23:45:21 +0500

    1- kun boshlangich masala

---
+++ js/main.js
@@ -0,0 +1,300 @@
+// let name = prompt("Ismingizni kiriting");
+// let age = prompt("Yoshingiz kiriting");
+// if (age >= 18) {
+//     alert("Kirishingiz mumkin")
+// } else if (age >= 18) {
+//     alert("Kirishingiz mumkin emas")
+// } else {
+//     alert("Kirishingiz mumkin emas")
+// }
+
+
+// yoshi 18 danyuqorilar saytga kiradikichiklar kiromidi
+
+// -------------------------------------------------
+// let number = 20;
+// if (number % 2 == 0) {
+//     console.log("JUFT")
+
+// } else {
+//     console.log("TOQ")
+// }
+
+// let
+
+
+// sonni juft yoki toqligini chiqarish
+
+
+// ----------------------------------------------------
+
+// let number = "-0";
+// if (number > 0) {
+//     console.log("Musbat")
+// } else if (number == 0 && number == -0) {
+//     console.log("NULL")
+// } else if (number < 0) {
+//     console.log("Manfiy")
+// } else {
+//     console.log("son kiriting")
+// }
+
+
+// sonni manfiy yoki musbatligini topish
+
+// -----------------------------------------------------
+
+
+// console.log(` 
+// ⠀     ⣀⡤⠤⠴⢒⠚⠒⠒⠦⢤⣀⠀⠀⠀⠀⠀⠀⠀⠀           ⠢⠤⡀
+// ⠀⠀⠀⣰⠋⣡⣶⣶⣷⣶⣶⣿⣿⣿⣷⣾⣽⣒⢤⡀⠀⠀⠀⠀    ⠀⠀⠀⣠⡤⠒⠂⢀⣈⣉⠉⠑⠒⠢⠤⡀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⢰⢣⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣆⠀⠀⠀    ⠀⠀⡰⠉⢁⣰⣶⣾⣿⣿⣿⣿⣷⣶⣼⣶⣼⣆⠀⠀⠀⠀
+// ⠀⠀⡼⣿⣿⣿⡿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀     ⠀⢠⣁⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⡀⠀⠀⠀
+// ⠀⢠⣧⣿⠋⠁⠀⠀⠀⠀⠈⠉⠉⠙⠛⠛⠿⣿⣿⣿⣿⣿⣧⠀    ⠀⣿......⠛⠉⠉⠁⠀⠈⠉⠉⠙⠻⣿⣿⣷⣿⡄⠀⠀⠀⠀
+// ⠀⢸⣽⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⡟⡇     ⠀⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣷⣿⢱⠀⠀⠀
+// ⠀⢸⠇⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣷⢱     ⠀⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣟⠂⡇⠀⡄
+// ⠀⢸⠀⠈⣉⣙⣿⡆⠀⠀⣰⣿⣟⢛⣛⣶⣄⠀⠀⢨⣿⣿⣿⡎     ⠀⣿⣿⠀⢀⣤⣶⣶⣦⠀⠀⢠⣴⣿⣷⣶⡄⠀⠘⢿⣿⣧⡇⠀⠿
+// ⠀⣼⠀⠘⠉⢉⠙⠁⠀⠀⠛⠛⣙⢛⡛⠻⠿⠄⠀⢸⣿⣿⣿⠇     ⢸⣿⡏⠀⠠⡶⢽⡶⠿⠃⠀⠘⢿⠿⠶⠟⠿⠂⠀⢸⣿⣿⡿⡀⠀
+// ⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣉⣙⡆     ⡜⢿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠟⠋⠀⡇⠀
+// ⢸⠀⠀⠀⠀⠀⠤⢠⣶⣦⡤⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠛⡇     ⢱⠈⠙⠀⠀⠀⠀⣠⢠⣤⣤⣤⣤⣤⡤⡀⠀⠀⠀⠀⠀⣤⠀⢰⣦
+// ⡸⠀⠀⠀⠀⢀⣀⡘⣛⡉⠉⠀⠀⠀⠀⠀⠀⠀⠀⢀⠈⠉⢠⡇     ⢸⠀⢢⡄⠀⠀⠴⣧⣴⡶⢿⢿⣿⣿⣿⣾⣦⠀⡀⢠⣆⠀⠙⠸⡿
+// ⠹⡄⠀⠀⠰⠿⢋⠉⠋⣛⣿⡷⠆⠀⠀⠀⠀⠀⠀⣬⣀⣠⠞⠀     ⢠⠀⠈⣷⣀⡀⠨⣿⢿⢋⣉⣉⣉⣙⡿⣿⡿⣠⣟⣾⡿⣤⡠⠂⣿
+// ⠀⢳⠀⠀⠀⠀⣀⣀⣠⣤⡶⠂⠀⠀⠀⠀⠀⠀⠀⣿⡟⠀⠀⠀     ⠘⠦⠤⣿⣿⣷⣼⣿⠀⠉⠉⠉⠉⠉⠀⢸⣿⣿⣿⣿⣿⠁⠀
+// ⠀⠘⣆⠀⠀⠀⠀⠉⠉⠀⠀⠀⠀⠀⢀⣀⣤⣶⣾⠿⡇⠀⠀⠀      ⠀⠀⠀⢸⢻⣿⣿⣿⣶⡀⠾⣦⣷⠔⢧⣾⣿⣿⣿⣿⢸⠀⠀⠀
+// ⠀⠀⠈⣦⠀⠀⣀⣀⣀⣤⣤⣴⣶⣿⣿⣿⣿⣿⣿⠀⣇⠀⠀⠀      ⠀⠀⢀⣾⠀⠉⣿⣿⣿⣧⣦⣻⣳⣶⣿⣿⣿⣾⣿⡿⢸⠀⠀⠀
+// ⠀⠀⠀⢹⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠠⠿⢤⡀⠀      ⠀⢀⣿⣿⡀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⢸⣧⠀⠀
+// ⠀⠀⠀⢸⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠑⠀⢱⡀      ⠀⢸⣿⣿⣇⠀⠈⠿⢿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠀⠀⣿⣿⡆⠀
+// ⠀⠀⢀⡸⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⡄⣼⡇      ⠀⠈⢿⣿⣿⡄⠀⠀⠀⠉⠛⠛⠛⠛⠉⠁⠀⠀⠀⠀⣿⣿⣇⠀
+// ⠀⡴⠋⠀⠀⠀⠀⠀⠀⢸⣿⣿#byFCA⣿⣿⣷⠀⣰⡝⢸⠇     ⠀⠀⢸⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⠀⠀
+// ⠀⠘⡆⠀⠀⠀⠀⠀⢀⣤⣤⣿⣿⣿⣿⣿⣿⡿⠃⣴⡟⢀⠎⠀       ⠀⠀⠀⣿⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⡇⠀⠀
+// ⠀⠀⠈⢧⠀⠹⣦⡀⠈⠻⣿⣿⣿⡿⠋⣴⡾⠛⢀⠜⠁⠀⠀⠀      ⠀⠀⠀⠀⠀⠙⠿⢿⣿⣿⣶⣄⡀⠀⠀⠀⠀⣰⣿⣿⠋⠀⠀⠀
+// ⠀⠀⠀⠈⢷⡀⠈⢻⣦⡀⠘⠋⢁⣴⡿⠋⢀⡴⠁⠀⠀⠀⠀⠀      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣦⠀⠀⠀⣿⣿⠃⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠻⣦⡀⠙⠟⢀⣴⠟⠉⣠⠔⠉⠀⠀⠀⠀⠀
+//         ⠙⣿⣿⣷⣤⣺⣿⡇⠀
+
+
+// `);
+
+
+// ---------------------------------------------------------------
+
+
+// let name = prompt("Ismingizni kiriting");
+// let age = prompt("Yoshingiz kiriting");
+// let vaqti = 18 -age ;
+// if (age >= 18) {
+//     alert("Siz prava olishiz mumkin ")
+// } else if (age <= 18) {
+// alert(`Siz prava olishingizga,${ vaqti} yetmayapti`)
+
+// }
+
+// prava olishga etaDIgan vaqt
+
+
+// --------------------------------------------------------
+
+
+
+
+// let age = prompt("Yilingizni kiriting");
+
+// if (2023 <= 2000 ||1990 >= 2000 ) {
+//         alert("Siz 21 asrda tugilgansz")
+//     }else if (2000  >= 1900) {
+//             alert("Siz 20 asrda tugilgansz")
+//         }
+
+
+
+
+
+
+
+// --------------------------------------------------------
+// let ball = prompt("Balingizni kiriting");
+
+
+// if(ball >= 90 && 100 ){
+//      alert("Siz grantdasiz")
+// }else if(ball >= 70 && 90){
+//        alert("Siz kontraktdasiz")
+// }else if(ball >= 50 && 70){
+//         alert("Siz superdasiz") 
+// }else {
+//         alert("Fail") 
+// };
+
+// ------------------------------------------------------------
+
+// let yil=prompt("Yilni kiriting");
+// if(yil%4===0)
+// if(yil%100===0)
+// if(yil%400===0){
+
+//         alert("Kabissa yili");
+// }else{
+//         alert("Kabissa yili emas");
+// }else{
+//         alert("Kabissa yili");
+// }else{
+//         alert("Kabissa yili emas");
+// }
+
+
+
+// kabisa yilini topish
+
+
+// ----------------------------------------------------------------
+// let currency_uzs = prompt("Dollar valyutasini kiriting");
+// let CURRENCY_USD = 11305;
+// alert(currency_uzs * CURRENCY_USD);
+
+
+// dollarni so'mga aylantirish
+
+
+// --------------------------------------------
+
+
+// let str = "Hello world!"
+// console.log(str.length);
+
+// stringni uzunligini topib beradi
+
+
+// --------------------------------------
+
+// let str = "Hello world!"
+// console.log(str.slice(1,-1));
+
+// selice  bu 1dan boshlab oxirigacha qirqib beradi
+
+// -------------------------------------------
+
+// let str = "Hello world!"
+// console.log(str.substring(6,11));
+
+// 6 chi indexdan boshlab 11 gacha qirqib beradi
+
+// ----------------------------------------------
+
+
+// let str = "Hello world!"
+// console.log(str.toLowerCase());
+
+
+
+// berilgan matnni kichkina qilib beradi
+
+// -------------------------------------------
+
+
+
+// let str = "Hello world!"
+// console.log(str.toUpperCase());
+
+
+
+// berilgan matnni katta qilib beradi
+// ----------------------------------------------------
+
+// let str = "Hello world!"
+// let eshmat ="TOshmat"
+// console.log(str.concat("Salom",eshmat));
+
+
+// matnni joyini o'zgartirib qo'yish
+
+// ------------------------------------------------
+
+// let str = "Hello world!"
+// console.log(str.charAt(1));
+
+
+// nolinchi indexdagi qiymatni olib beradi
+
+// -----------------------------------------------------
+
+// let str = "Hello world!"
+// console.log(str.charCodeAt(0));
+
+// ----------------------------------------------------------------
+
+
+// let out="<<<>>>",word="WooHoo";
+
+// console.log(out.slice(0 , out.length /2)+word+out.slice(out.length / 2));
+
+// qo'shtirnoni ichiga solvorish
+
+// ----------------------------------------------------
+
+// let a="hello";
+// console.log(a.substring(2)+a.slice(0,2));
+
+
+// olleh  holatida chiqadi 
+// ohiridagi ikkita harfni boshiga qo'shib beradi
+
+// **********************************************************
+
+
+// let middle="code"
+// console.log(middle.slice(middle.length / 2-1,middle.length / 2+1));
+
+
+// faqat matinnni o'rtadagi harflarini oladi
+
+// ---------------------------------------------------
+
+
+// let a="Hello", n=2;
+// console.log(a.slice(0,n)+a.slice(-n));
+
+
+
+// -----------------------------------------------------
+// let a="hello";
+// console.log(a.substring(2)+a.slice(0,2));
+
+
+// olleh  holatida chiqadi 
+// ohiridagi ikkita harfni boshiga qo'shib beradi
+
+// --------------------------------------------------------++
+
+
+// let title = "NajoT taLim ABullo";
+// let result = title.match(/[A-Z]/g).join("")
+// console.log(result);
+
+// bu yozilgan matinni faqat katta harflarini olib beradi
+
+// -------------------------------------------------------------
+
+
+// let str = "Hello world!"
+// console.log(str.slice(1,-1));
+
+// selice  bu 1dan boshlab oxirigacha qirqib beradi
+// --------------------------------------------------------------------
+
+// let title = "Najot talimdan ";
+// console.log(title.match(/a/gi).length+"ta bor")
+
+
+
+// nechta a borligini hisoblash
+
+// ----------------------------------------------------------------
+
+
+// let title = "Najot talimdan hjjv hsgvchgsd";
+// console.log(title.split(" ").length);
+
+// ###############################################################
+
+
+
